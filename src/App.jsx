fa0d4bb5b08ec655b3c6baf32df8e43c4d752dd7
Marek <marek@ilieth.at>
2022-08-16 13:44:42 +0200

    Fix dot not being treated as new value when dot was pressed after pressing operation button + refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import './App.css';
 import BUTTONS from './BUTTONS/BUTTONS';
 import RESULT from './RESULT/RESULT';
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
+
+const press_button_reducer = (state, action) => {
+  switch (action.type) {
+    case 'press':
+      return {...state, button_being_pressed: action.value};
+    case 'animate':
+      return {...state, button_being_animated: action.value};
+    default:
+      return state;
+  }
+}
 
 const calculation_reducer = (state, action) => {
   switch (action.type) {
@@ -17,136 +28,123 @@ const calculation_reducer = (state, action) => {
 }
 
 const App = () => {
-  const [button_being_pressed, set_button_being_pressed] = useState(0);
-  const [button_being_animated, set_button_being_animated] = useState(null);
-  const [state, dispatch] = useReducer(calculation_reducer, {
+  const [press_button_state, press_button_dispatch] = useReducer(press_button_reducer, {
+    button_being_pressed: 0,
+    button_being_animated: null,
+  })
+  const [calculation_state, calculation_dispatch] = useReducer(calculation_reducer, {
     value_to_display: 0, 
     current_operation: null, 
     value_to_count: null,
   })
 
   useEffect(() => {
-    console.log(state)
-  }, [state])
+    console.log(calculation_state)
+  }, [calculation_state])
 
   useEffect(() => {
-    const was_number_or_dot_or_equals_pressed = Number(button_being_pressed) || button_being_pressed === '0' || button_being_pressed === '.' || button_being_pressed === '=';
+    const was_number_or_dot_or_equals_pressed = 
+      Number(press_button_state.button_being_pressed) || 
+      press_button_state.button_being_pressed === '0' || 
+      press_button_state.button_being_pressed === '.' || 
+      press_button_state.button_being_pressed === '=';
     
     const display_value = (pressed_button) => {
-      const was_number_pressed = Number(pressed_button) || pressed_button === '0';
-      const is_any_dot_on_display = String(state.value_to_display).split('.').length > 1;
-      const is_only_zero_displayed = !state.value_to_display;
+      const is_any_dot_on_display = String(calculation_state.value_to_display).split('.').length > 1;
+      const is_only_zero_displayed = !calculation_state.value_to_display;
+      const was_number_or_dot_pressed_after_pressing_operation = calculation_state.current_operation && pressed_button !== '=';
 
-      if (was_number_pressed) {
-        if (state.current_operation) {
-          const clear_display_and_return_new_number_to_display = () => {
-            dispatch({type: 'count', value: state.value_to_count + state.current_operation})
-            dispatch({type: 'operation', value: null})
-            return button_being_pressed;
-          }
-          return clear_display_and_return_new_number_to_display();
+      if (was_number_or_dot_pressed_after_pressing_operation) {
+        const clear_display_and_return_new_number_to_display = () => {
+          calculation_dispatch({type: 'count', value: calculation_state.value_to_count + calculation_state.current_operation});
+          calculation_dispatch({type: 'operation', value: null});
+          return press_button_state.button_being_pressed;
         }
+        return clear_display_and_return_new_number_to_display();
 
-        const display_next_number = () => {
-          return is_only_zero_displayed ? pressed_button : state.value_to_display + pressed_button;
-        }
+      } else if (pressed_button === '=') {
+        const were_both_numbers_to_calculate_inputed = typeof calculation_state.value_to_count === 'string';
+        return were_both_numbers_to_calculate_inputed ? do_operation() : calculation_state.value_to_display;
 
-        return display_next_number();
-      } else {
-        if (pressed_button === '=') {
-          const were_both_numbers_to_calculate_inputed = typeof state.value_to_count === 'string';
-          
-          if (were_both_numbers_to_calculate_inputed) {
-            return do_operation();
-          } else if (!state.current_operation) {
-            return state.value_to_display;
-          }
-
-          return Number(state.value_to_count);
-        } else {
-          if (is_any_dot_on_display) { // prevent more than one dots
-            return state.value_to_display;
-          }
-          return state.value_to_display + pressed_button;
-        }
+      } else if (pressed_button === '.') {
+        return is_any_dot_on_display ? calculation_state.value_to_display : calculation_state.value_to_display + pressed_button;
       }
+
+      return is_only_zero_displayed ? pressed_button : calculation_state.value_to_display + pressed_button;
     }
 
     const change_display_value = () => {
-      dispatch({
+      calculation_dispatch({
         type: 'display', 
-        value: display_value(button_being_pressed)
+        value: display_value(press_button_state.button_being_pressed)
       });
-      set_button_being_pressed(null);
+      press_button_dispatch({type: 'animate', value: null});
     }
 
     const do_operation = () => {
-      const is_at_least_third_number_inputed_or_equals_pressed = state.value_to_count && !Number(state.value_to_count);
+      const first_two_inputed_numbers_were_calculated_and_next_number_will_be_inputed = calculation_state.value_to_count && !Number(calculation_state.value_to_count);
 
-      if (is_at_least_third_number_inputed_or_equals_pressed) {
+      if (first_two_inputed_numbers_were_calculated_and_next_number_will_be_inputed) {
         const calculate_before_displaying_new_value = () => {
-          const operation = state.value_to_count.slice(state.value_to_count.length - 1);
-          const present_display_value = Number(state.value_to_display);
-          const past_display_value = Number(state.value_to_count.slice(0, -1));
+          const operation = calculation_state.value_to_count.slice(calculation_state.value_to_count.length - 1);
+          const present_display_value = Number(calculation_state.value_to_display);
+          const past_display_value = Number(calculation_state.value_to_count.slice(0, -1));
           let value_to_display;
   
           const dispatches = (value) => {
-            dispatch({type: 'count', value});
-            dispatch({type: 'display', value});
+            calculation_dispatch({type: 'count', value});
+            calculation_dispatch({type: 'display', value});
           }
 
           switch(operation) {
             case '+':
               value_to_display = past_display_value + present_display_value;
-              dispatches(value_to_display);
               break;
             case '-':
               value_to_display = past_display_value - present_display_value;
-              dispatches(value_to_display);
               break;
             case '*':
               value_to_display = past_display_value * present_display_value;
-              dispatches(value_to_display);
               break;
             case '/':
               value_to_display = past_display_value / present_display_value;
-              dispatches(value_to_display);
               break;
             default:
               return false;
           }
+
+          dispatches(value_to_display);
         }
 
         calculate_before_displaying_new_value();
       } else {
         const remember_value_displayed_before_operation = () => {
-          dispatch({type: 'count', value: Number(state.value_to_display)});
+          calculation_dispatch({type: 'count', value: Number(calculation_state.value_to_display)});
         }
-
         remember_value_displayed_before_operation();
       }
-      dispatch({type: 'operation', value: button_being_pressed});
+      calculation_dispatch({type: 'operation', value: press_button_state.button_being_pressed});
     }
 
     if (was_number_or_dot_or_equals_pressed) {
       change_display_value();
-    } else if (button_being_pressed) {
+    } else if (press_button_state.button_being_pressed) {
       do_operation();
     }
     
-    set_button_being_animated(button_being_pressed);
-    setTimeout(() => {set_button_being_animated(null)}, 200);
-  }, [button_being_pressed]);
+    press_button_dispatch({type: 'animate', value: press_button_state.button_being_pressed});
+    setTimeout(() => {press_button_dispatch({type: 'animate', value: null});}, 200);
+  }, [press_button_state.button_being_pressed]);
 
   const handle_press_button = (e) => {
-    set_button_being_pressed(e.target.textContent);
+    press_button_dispatch({type: 'press', value: e.target.textContent});
   }
 
   return (
     <div className="App">
       <div className="calculator-container">
-        <RESULT value_to_display={state.value_to_display}/>
-        <BUTTONS button_being_animated={button_being_animated} press_button={handle_press_button}/>
+        <RESULT value_to_display={calculation_state.value_to_display}/>
+        <BUTTONS button_being_animated={press_button_state.button_being_animated} press_button={handle_press_button}/>
       </div>
     </div>
   );
